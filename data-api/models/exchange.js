56436fb6013c7d168a916b814f01bad7ff2bec2a
danivsainz <danivsainz@gmail.com>
2018-03-30 23:23:07 -0400

    Begin modifying whole backend

--- data-api/models/exchange.js
+++ data-api/models/exchange.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 
 const ExchangeSchema =  mongoose.Schema({
-    name: {type: String, required: true, unique:true}
+    name: {type: String, required: true, unique:true},
+    volume_24h:{type:String}
 });
 
 const Exchange = module.exports = mongoose.model('Exchange', ExchangeSchema);
