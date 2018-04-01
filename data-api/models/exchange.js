1db31b354b8c876a1fc12f857e64f0d9b7d1fd06
danivsainz <danivsainz@gmail.com>
2018-04-01 11:43:37 -0400

    Create market schema

--- data-api/models/exchange.js
+++ data-api/models/exchange.js
@@ -4,6 +4,6 @@ const mongoose = require('mongoose');
 const ExchangeSchema =  mongoose.Schema({
     name: {type: String, required: true, unique:true},
     volume_24h:{type:String}
-});
+},{timestamps:true});
 
 const Exchange = module.exports = mongoose.model('Exchange', ExchangeSchema);
