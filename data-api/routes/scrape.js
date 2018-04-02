cfcee5bf2107eefd81d249716430cbb0fb8d4516
danivsainz <danivsainz@gmail.com>
2018-04-02 02:06:19 -0400

    remove old method begin impl update coin

--- data-api/routes/scrape.js
+++ data-api/routes/scrape.js
@@ -19,45 +19,6 @@ router.get('/getall', async (req,res,next)=>{
     }
 });
 
-router.get('/scrape/:id', async (req,res,next) => {
-    try{
-        let exchange = await Exchange.findOne({name: req.params.id})
-        const url = "https://api.coinmarketcap.com/v1/ticker/";
-        const response = await axios.get(url);
-        console.log(response.data);
-        const data = response.data;
-        let counter = 0;
-        data.forEach(element => {
-            element.rank = parseInt(element.rank);
-            let coin = new CryptoCurrency({
-                name: element.name,
-                symbol: element.symbol,
-                rank: element.rank,
-                price_usd: element.price_usd,
-                price_btc: element.price_btc,
-                '24h_volume_usd': element['24h_volume_usd'],
-                market_cap_usd: element.market_cap_usd,
-                available_supply: element.available_supply,
-                total_supply: element.total_supply,
-                max_supply: element.max_supply,
-                percent_change_1h: element.percent_change_1h,
-                percent_change_24h: element.percent_change_24h,
-                percent_change_7d: element.percent_change_7d,
-                last_updated: element.last_updated,
-                exchange_id: exchange._id,
-                coin_exchange: (element.name + exchange.name).replace(/\s/g,'') 
-                });
-            coin.save(err=>{
-                if (err) console.log(err);
-                console.log(coin.name);
-            });
-        });
-        res.status(200).send({text:"finished"});
-    }catch(err){
-        next(err)
-    }
-});
-
 router.get('/coinmarketcap', async (req,res,next) => {
     try{
         let coinMarketCap = await Exchange.coinMarketCap();
@@ -99,6 +60,29 @@ router.get('/coinmarketcap', async (req,res,next) => {
                     if (err) console.log(err);
                     saved++;
                 });
+            }else{
+                if(coin.length == 1){
+                    coin.set({
+                        name: data[i].name,
+                        symbol: data[i].symbol,
+                        rank: data[i].rank,
+                        price_usd: data[i].price_usd,
+                        price_btc: data[i].price_btc,
+                        '24h_volume_usd': data[i]['24h_volume_usd'],
+                        market_cap_usd: data[i].market_cap_usd,
+                        available_supply: data[i].available_supply,
+                        total_supply: data[i].total_supply,
+                        max_supply: data[i].max_supply,
+                        percent_change_1h: data[i].percent_change_1h,
+                        percent_change_24h: data[i].percent_change_24h,
+                        percent_change_7d: data[i].percent_change_7d,
+                        last_updated: data[i].last_updated,
+                    });
+                    coin.save(err=>{
+                        if (err) console.log(err);
+                        saved++;
+                    });
+                }
             }
         }
         
