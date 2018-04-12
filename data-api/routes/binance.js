5370153708065a81be465e64bca1660eccf1cc3c
danivsainz <danivsainz@gmail.com>
2018-04-12 05:05:43 -0400

    begin fleshing out binance route

---
+++ data-api/routes/binance.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const CryptoCurrency = require('../models/cryptoCurrency');
+const Exchange = require('../models/exchange');
+
+
+router.get('/coinmarketcap/getpairdata/:symbol', async (req,res,next) => {
+    try{
+
+    }catch(err){
+
+    }
+});
+
+
+module.exports = router;
+
+
