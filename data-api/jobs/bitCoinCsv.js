c06eb880180e1cacdbae836a65c19dbae8af707e
danivsainz <danivsainz@gmail.com>
2018-04-12 04:43:33 -0400

    finish csv parser

--- data-api/jobs/bitCoinCsv.js
+++ data-api/jobs/bitCoinCsv.js
@@ -13,38 +13,38 @@ mongoose.Promise = require("bluebird");
 // mongoose.connect(process.env.mongoUrl);
 mongoose.connect("mongodb://localhost:27017/cryptoNalysisApi");
 
-
 const saveToDb = async () => {
   try {
     await mongoose.connection.on("connected", () => {
       console.log("Connected to Database " + process.env.mongoUrl);
     });
-    let saved;
-    let exchange = await Exchange.findOne({name:'Binance'});
+    let saved = 0;
+    let exchange = await Exchange.findOne({ name: "Binance" });
     console.log(exchange);
-    let market = await Market.findOne({exchange_id:exchange._id, symbol:'USDT'});
-    console.log(market)
-    let pair = await Pair.findOne({market_id:market._id, pair:'BTCUSDT'});
+    let market = await Market.findOne({
+      exchange_id: exchange._id,
+      symbol: "USDT"
+    });
+    console.log(market);
+    let pair = await Pair.findOne({ market_id: market._id, pair: "BTCUSDT" });
     console.log(pair);
     let day;
     let dataDay;
 
-
     let parser = await parse({ delimiter: "," }, async function(err, data) {
-      for(let i=0;i<data.length;i++){
-        if(i!=0){
-          dataDay=data[i];
-          day = await Day.findOne({date:data[i][0],pair_id:pair._id});
-          if(i==1){
-            console.log('inside 1 ')
+      for (let i = 0; i < data.length; i++) {
+        if (i != 0) {
+          dataDay = data[i];
+          day = await Day.findOne({ date: data[i][0], pair_id: pair._id });
+          if (day == undefined) {
             day = new Day({
               // 0"Date",1"Price",2"Open",3"High",4"Low",5"Vol.",6"Change %"
-              date:dataDay[0],
-              openingPrice:dataDay[1],
-              lowestPrice:dataDay[4],
-              highestPrice:dataDay[3],
-              changePercent:dataDay[6],
-              totalVolume:dataDay[5],
+              date: dataDay[0],
+              openingPrice: dataDay[1],
+              lowestPrice: dataDay[4],
+              highestPrice: dataDay[3],
+              changePercent: dataDay[6],
+              totalVolume: dataDay[5],
               pair_id: pair._id
             });
             day = await day.save();
@@ -58,16 +58,9 @@ const saveToDb = async () => {
     });
 
     await fs.createReadStream(__dirname + "/my-csv.csv").pipe(parser);
-
-
-    return saved;
   } catch (error) {
     console.log(error);
   }
 };
 
-saveToDb().then((saved)=>{
-  console.log(saved);
-})
-
-
+saveToDb();
